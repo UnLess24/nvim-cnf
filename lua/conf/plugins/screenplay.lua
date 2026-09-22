6b204c80c73456616e865f5afd163b0969a52372
Khaydanov Yuriy <y.khaydanov@crpt.ru>
2026-09-22 11:54:52 +0300

    delete autocmd to start Skreenkey when VimEnter

--- lua/conf/plugins/screenplay.lua
+++ lua/conf/plugins/screenplay.lua
@@ -3,10 +3,5 @@ return {
   lazy = false,
   version = "*", -- or branch = "main", to use the latest commit
   config = function()
-    vim.api.nvim_create_autocmd('VimEnter', {
-      callback = function()
-        vim.cmd("Screenkey")
-      end,
-    })
   end,
 }
